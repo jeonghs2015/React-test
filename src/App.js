0a8280ece7c3341a44836109b0b000dbe2871336
hsjung2015 <hsjung201558@gmail.com>
2022-08-30 14:18:57 +0900

    useState 사용하여 값 변경하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function Header(props) {
   return <header>
@@ -31,16 +32,28 @@ function Article(props){
 }
 
 function App() {
+  //const _mode = useState('WELCOME');
+  //const mode = _mode[0];
+  //const setMode = _mode[1];
+  const [mode, setMode] = useState('WELCOME');
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, World!!!"></Article>
+  } else if(mode === 'READ'){
+    content = <Article title="Read" body="Hello, Read!!!"></Article>
+  } else{
+    content = <Article title="Welcome" body="Hello, World!!!"></Article>
+  }
   return (
     <div>
-      <Header title="WEB" onChangeMode={()=>{alert('Header');}}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{alert(id)}}></Nav>
-      <Article title="Welcome" body="Hello, World!!!"></Article>
+      <Header title="WEB" onChangeMode={()=>{setMode('WELCOME');}}></Header>
+      <Nav topics={topics} onChangeMode={(id)=>{setMode('READ');}}></Nav>
+      {content}
     </div>
   );
 }

abb203247ee41efad469d07c567dcb9a7565e12f
hsjung2015 <hsjung201558@gmail.com>
2022-08-30 11:43:39 +0900

    Nav 에 Event 추가

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ function Nav(props) {
   const lis = []
   for(let i=0;i<props.topics.length;i++){
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+    }}>{t.title}</a></li>)
   }
   return <nav>
             <ol>
@@ -36,7 +39,7 @@ function App() {
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{alert('Header');}}></Header>
-      <Nav topics={topics}></Nav>
+      <Nav topics={topics} onChangeMode={(id)=>{alert(id)}}></Nav>
       <Article title="Welcome" body="Hello, World!!!"></Article>
     </div>
   );
